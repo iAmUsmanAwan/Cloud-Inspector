be8644a7fb6ab0dcd39382b4cc0797d17f45a16a
Umer <awan.umer.m@gmail.com>
2025-05-04 16:46:09 +0500

    First Solution Created
    1. Solution added
    2. First Console app added
    3. Updated ReadMe

---
+++ Cloud_Inspector/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cloud_Inspector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+--First Project Added
+
 --User Added (Umer-Afzal-Awan) (May 04, 2025)
 
 # Cloud-Inspector
